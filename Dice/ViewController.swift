1d104bfe04e90ff3cef7b032807f3c354495b921
letJamie <stillbut@naver.com>
2020-06-12 15:26:08 +0900

    Add random function

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -14,14 +14,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView2: UIImageView!
     
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView1.alpha = 0.5
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+            
+        var diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        
+        
+        
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
 
+    
+    }
+    
+    
+    
 }
 
